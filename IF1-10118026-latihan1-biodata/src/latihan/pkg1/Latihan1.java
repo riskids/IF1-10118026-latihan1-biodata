eed8872b91671e0df933caffb9a036118ea90078
Riski Dwi <bintangmalamhari@gmail.com>
2019-09-26 12:33:43 +0700

    Add files via upload

---
+++ IF1-10118026-latihan1-biodata/src/latihan/pkg1/Latihan1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg1;
+
+/**
+ *
+ * @author dwisr
+ * NAMA      : RISKI DWI SABARIYANTO
+ * KELAS     : IF -1 
+ * NIM       : 10118026
+ * Deskripsi : Program untuk menampilkan teks
+ * 
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" +"\n"+"---------------------");
+        System.out.println("NAMA\t\t: RISKI DWI SABARIYANTO");
+        System.out.println("ALAMAT\t\t: Jl.Babakansari No 89");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 21 Maret 2000");
+        System.out.println("----------------------\t\t:");
+        System.out.println("HOBI\t\t: Photography");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+    
+}
